1f6d11bccca3ec86006ed05cd56c940203d47828
Gabriel Rotger <rotgergabriel@gmail.com>
2022-10-17 19:02:32 +0200

    Proyecto finalizado

--- script.js
+++ script.js
@@ -1,53 +1,31 @@
-( () => {
+import checkComplete from "./components/checkComplete.js"
+import deleteIcon from "./components/deleteIcon.js"
 
-    const btn = document.querySelector('[data-form-btn]')
+const btn = document.querySelector('[data-form-btn]')
 
-    const createTask = (event) => {
-        event.preventDefault()
-        const input = document.querySelector('[data-form-input]')
-        const value = input.value
-        const list = document.querySelector('[data-list]')
-        const task = document.createElement('li')
-        const taskContent = document.createElement('div')
-        const titleTask = document.createElement('span')
-        titleTask.classList.add('task')
-        titleTask.innerHTML = value
+const createTask = (event) => {
+    event.preventDefault()
+    const input = document.querySelector('[data-form-input]')
+    const value = input.value
+    const list = document.querySelector('[data-list]')
+    const task = document.createElement('li')
+    const taskContent = document.createElement('div')
+    const titleTask = document.createElement('span')
+    titleTask.classList.add('task')
+    titleTask.innerHTML = value
 
-        taskContent.appendChild(checkComplete())
-        taskContent.appendChild(titleTask)
-        taskContent.appendChild(checkDelete())
-
-        task.classList.add('card')
-        input.value = ''
-        
-        const content =`
-        <i class="fas fa-trash-alt trashIcon icon data-delete"></i>`
-        
-        list.appendChild(task)
-        task.appendChild(taskContent) 
-    }
-
-    btn.addEventListener('click', createTask)
-
-    const checkComplete = () => {
-        const i = document.createElement('i')
-        i.classList.add('far', 'fa-check-square', 'icon')
-        i.addEventListener('click', completeTask)
-        return i
-    }
-
-    const completeTask = (event) => {
-        const element = event.target
-        element.classList.toggle('far')
-        element.classList.toggle('fas')
-        element.classList.toggle('completeIcon')
-    }
-
-
-    const checkDelete = () => {
-        const i = document.createElement('i')
-        i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon', 'data-delete')
-        return i
-    }
+    taskContent.appendChild(checkComplete())
+    taskContent.appendChild(titleTask)
     
-})()
+    task.classList.add('card')
+    input.value = ''
+    
+    const content =`
+    <i class="fas fa-trash-alt trashIcon icon data-delete"></i>`
+    
+    task.appendChild(taskContent) 
+    list.appendChild(task)
+    task.appendChild(deleteIcon())
+}
+
+btn.addEventListener('click', createTask)
